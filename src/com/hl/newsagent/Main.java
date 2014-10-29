fdd36e382e64ce2fba9a47e4b125b46801e256a1
Henry Lim <henry.lim29@gmail.com>
2014-10-29 23:07:12 +1100

    Version One! -CSV file -Better menu

--- src/com/hl/newsagent/Main.java
+++ src/com/hl/newsagent/Main.java
@@ -5,29 +5,56 @@ public class Main {
 	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("NewsAgent by Henry L , UTS, 2014. Version v0.9");
-		NewsParser news = new NewsParser(readURL());
+		System.out.println("NewsAgent by Henry L , UTS, 2014. Version v1.0");
+		String input = readURL();
+		String[] inputs = input.split(" ");
+		
+		NewsParser news = new NewsParser(inputs[0]);
+		
+		boolean showRankAndId = askIdRank();
+		boolean showExcluded = askExclusionList();
+		boolean showFrequent = askFrequentList();
+		
+		
 		if (news.isUrlValid()){
+			
+			news.populateSentencesList();
 			news.createWordCount();
-			news.sortFrequentWords();
-			
+			news.processFrequentWords();
+			news.processHeadline();
 			news.calculateRelevance();
+			news.printMostRelevant(showRankAndId);
 			
-			news.printMostRelevant();
-			
-			//System.out.print(news.)
-			//news.printContents();
+			if (showExcluded){
+				news.printExcludedWordList();
+			}
+			if (showFrequent){
+				news.printFrequentWordList();
+			}
 		}
 		else {
 			System.out.println("The URL is invalid, please try again");
 			sc.nextLine();
-			main(args);
 		}
-			
+	}
+	
+	private static boolean askIdRank(){
+		System.out.print("Do you want the IDs and the rank of a sentence to be shown? (y/n) ");
+		return sc.nextLine().toLowerCase().equals("y");
+	}
+	
+	private static boolean askExclusionList(){
+		System.out.print("Do you want to also see the excluded keywords? (y/n) ");
+		return sc.nextLine().toLowerCase().equals("y");
+	}
+	
+	private static boolean askFrequentList(){
+		System.out.print("Do you want to also see the frequent words? (y/n) ");
+		return sc.nextLine().toLowerCase().equals("y");
 	}
 
 	private static String readURL() {
-		System.out.print("Please input a URL for me to parse: ");
+		System.out.print("Please input a URL to summarise: ");
 		return sc.nextLine();
 	}
 
--- src/com/hl/newsagent/NewsParser.java
+++ src/com/hl/newsagent/NewsParser.java
@@ -1,6 +1,9 @@
 package com.hl.newsagent;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -27,58 +30,87 @@ public class NewsParser {
 	private int uniqueWordCount;
 	private String headline;
 	private String htmlContent;
+	private String Url;
 	private Document parsedHtml;
-	private LinkedList<String> content;
 	private LinkedList<String> headlineKeywords = new LinkedList<String>();
 	private LinkedList<String> frequentWords = new LinkedList<String>();
+	private LinkedList<String> exclusionList = new LinkedList<String>();
 	private LinkedList<NewsFragment> contents = new LinkedList<NewsFragment>();
 	private Map<String, Integer> wordFreq = new HashMap<String, Integer>();
-	private LinkedList<String> words = new LinkedList<String>();
 
 	public NewsParser(String URL) {
+		if (!parseUrl(URL))
+			return;
+		else
+			this.Url = URL;
+		headline = parsedHtml.body().select("h1").first().text();
+	}
+
+	public void processHeadline() {
+		String splittedHeader[] = headline.split("[\\p{P} \\t\\n\\r]");
+
+		for (String s : splittedHeader) {
+			if (!exclusionList.contains(s))
+				headlineKeywords.add(s);
+		}
+		System.out.println();
+		System.out.println("HEADLINE: " + headline);
+		System.out.println();
+	}
+
+	public void populateSentencesList() {
+		Elements paragraphs = null;
+		paragraphs = retrieveHtmlContents(paragraphs);
+		if (paragraphs == null) {
+			System.out.println("Unable to extract the article");
+			return;
+		} else {
+			BreakIterator iterator = BreakIterator
+					.getSentenceInstance(Locale.US);
+			int id = 0;
+			for (Element e : paragraphs) {
+				iterator.setText(e.text());
+				int start = iterator.first();
+				for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator
+						.next()) {
+					NewsFragment n = new NewsFragment(e.text().substring(start,
+							end));
+					n.setId(id);
+					id++;
+					contents.add(n);
+				}
+
+			}
+		}
+	}
+
+	private boolean parseUrl(String URL) {
 		try {
 			parsedHtml = Jsoup.connect(URL).get();
 		} catch (IllegalArgumentException e) {
 			System.out.println("The URL is invalid: " + e.getMessage());
-			return;
+			return false;
 		} catch (MalformedURLException e) {
 			System.out.println("The URL is invalid: " + e.getMessage());
-			return;
+			return false;
 		} catch (HttpStatusException e) {
 			System.out.println("Error with getting the HTML: "
 					+ e.getStatusCode());
-			return;
+			return false;
 		} catch (IOException e) {
-			// e.printStackTrace();
+			// e.printStackTrace();\
+			return false;
 		}
-
-		System.out.println("HEADLINE: "
-				+ splitHeader(parsedHtml.body().select("h1").first().text()));
-
-		System.out.println();
-
-		Elements paragraphs = null;
-
-		paragraphs = retrieveHtmlContents(paragraphs);
-
-		BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
-		int id = 0;
-		for (Element e : paragraphs) {
-			iterator.setText(e.text());
-			int start = iterator.first();
-			for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator
-					.next()) {
-				NewsFragment n = new NewsFragment(e.text()
-						.substring(start, end));
-				n.setId(id);
-				id++;
-				contents.add(n);
-			}
-
-		}
-
+		return true;
 	}
 
+	/**
+	 * Loops through common HTML layouts in order to retrieve the news article
+	 * Returns null if no valid text is found
+	 * 
+	 * @param paragraphs
+	 * @return
+	 */
 	private Elements retrieveHtmlContents(Elements paragraphs) {
 		String[] htmlLayouts = { "div[class*=article] > p",
 				"div[class*=story] > p", "div[class*=main] > p", "article > p" };
@@ -89,15 +121,6 @@ public class NewsParser {
 			i++;
 		}
 		return paragraphs;
-	}
-
-	private String splitHeader(String header) {
-		String splittedHeader[] = header.split("[\\p{P} \\t\\n\\r]");
-
-		for (String s : splittedHeader) {
-			headlineKeywords.add(s);
-		}
-		return header;
 	}
 
 	public void calculateRelevance() {
@@ -125,11 +148,21 @@ public class NewsParser {
 			sentence = news.getSentence();
 			wordArray = sentence.split("[\\p{P} \\t\\n\\r]");
 			for (String s : wordArray) {
+				// Converts the string to lowercase
 				s = s.toLowerCase();
+
+				// Retrieves the string from the hashmap, if available
 				Integer val = wordFreq.get(s);
+
 				if (val != null) {
-					for (Entry<String, Integer> e : wordFreq.entrySet()){
-						if (e.getKey().startsWith(s.substring(0, s.length()/2))){
+					// If it's not available, loop through the map until it
+					// finds a word that
+					// begins with something similar
+					// E.g. 'working' begins with 'work', therefore part of the
+					// same word
+					for (Entry<String, Integer> e : wordFreq.entrySet()) {
+						if (e.getKey().startsWith(
+								s.substring(0, s.length() / 2))) {
 							wordFreq.put(s, new Integer(val + 1));
 							uniqueWordCount++;
 						}
@@ -137,14 +170,19 @@ public class NewsParser {
 					wordFreq.put(s, new Integer(val + 1));
 					uniqueWordCount++;
 				} else {
+					// If the word is available, increment the value in the
+					// hashmap
 					wordFreq.put(s, 1);
-					words.add(s);
 				}
 			}
 		}
 	}
 
-	public void sortFrequentWords() {
+	/**
+	 * Sorts the most used words
+	 * Also writes the frequent words into a CSV file
+	 */
+	public void processFrequentWords() {
 		ValueComparator bvc = new ValueComparator(wordFreq);
 		TreeMap<String, Integer> sorted_map = new TreeMap<String, Integer>(bvc);
 
@@ -152,14 +190,109 @@ public class NewsParser {
 
 		List<String> keys = new LinkedList<String>(sorted_map.keySet());
 
-		for (int i = 0; i < keys.size() / 5; i++) {
-			//if (keys.get(i).length() > 4) {
-				frequentWords.add(keys.get(i));
+		List<String> veryFrequentWords = new LinkedList<String>();
 
-				System.out.println(keys.get(i));
-			//}
+		for (int i = 0; i < keys.size() / 2; i++) {
+			veryFrequentWords.add(keys.get(i));
+		}
+		
+		writeToFile(veryFrequentWords);
+
+		for (int i = 0; i < keys.size() / 3; i++) {
+			if (!exclusionList.contains(keys.get(i)))
+				frequentWords.add(keys.get(i));
 		}
 
+	}
+
+	/**
+	 * This function creates a new CSV file. The data in this file is populated from the frequent words
+	 * in the current bot usage session
+	 * @param veryFrequentWords A LinkedList that contains the most frequently used words
+	 * @param csvFile The path to the CSV file
+	 */
+	private void createNewFile(List<String> veryFrequentWords, String csvFile) {
+		try {
+			FileWriter writer = new FileWriter(csvFile);
+
+			for (String s : veryFrequentWords) {
+				writer.append(s);
+				writer.append(",");
+				writer.append("1");
+				writer.append("\n");
+			}
+			writer.flush();
+			writer.close();
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	/**
+	 * Overwrites the CSV file with the new values. 
+	 * Increments the number alongside the word when the word is also used in the article
+	 * @param veryFrequentWords A LinkedList that contains the most frequently used words
+	 */
+	private void writeToFile(List<String> veryFrequentWords) {
+		//Variables setup
+		String csvFile = "./frequent.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+
+		try {
+
+			Map<String, Integer> maps = new HashMap<String, Integer>();
+
+			br = new BufferedReader(new FileReader(csvFile));
+
+			while ((line = br.readLine()) != null) {
+				//Delimit the line by comma, as it is a comma separated file
+				String[] wordEntry = line.split(cvsSplitBy);
+				int wordUsage = Integer.parseInt(wordEntry[1]);
+
+				//If the word in the CSV is also a frequent word in this article, then increment the frequency
+				//Otherwise, decrement it.
+				if (veryFrequentWords.contains(wordEntry[0]))
+					wordUsage++;
+				else
+					wordUsage--;
+
+				maps.put(wordEntry[0], wordUsage);
+			}
+
+			//Writes in the new words and values
+			FileWriter writer = new FileWriter(csvFile);
+			for (Map.Entry<String, Integer> entry : maps.entrySet()) {
+				if (entry.getValue() > 2) {
+					exclusionList.add(entry.getKey());
+				}
+				writer.append(entry.getKey());
+				writer.append(",");
+				writer.append(String.valueOf(entry.getValue()));
+				writer.append("\n");
+			}
+
+			//Close off the file IO connection
+			writer.flush();
+			writer.close();
+
+		} catch (FileNotFoundException e) {
+			// If no frequent word file exists, create a new one
+			createNewFile(veryFrequentWords, csvFile);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	/**
@@ -189,23 +322,44 @@ public class NewsParser {
 		}
 	}
 
-	public void printMostRelevant() {
+	/**
+	 * This method takes the five most relevant sentences, sorts it by its
+	 * written order, and prints it
+	 */
+	public void printMostRelevant(boolean showExtra) {
+		// Sort the contents list (the sentences) by its rank
 		Collections.sort(contents, new CompareByRank());
 
 		LinkedList<NewsFragment> tempCollection = new LinkedList<NewsFragment>();
 
-		for (int i = 0; i < 4; i++) {
+		// Take the first five relevant sentences
+		for (int i = 0; i < 5; i++) {
 			tempCollection.add(contents.get(i));
 		}
 
+		// Sort the sentences by its Id (i.e. its chronological order in the
+		// article)
 		Collections.sort(tempCollection, new CompareById());
 
 		for (NewsFragment n : tempCollection) {
-			System.out.println(n.getId() + " " + n.getRank() + " "
-					+ n.getSentence());
+			if (showExtra){
+				System.out.println(n.getId() + "\t" + n.getRank() + "\t" + n.getSentence());
+			} else {
+				System.out.println("\u2022 " + n.getSentence());
+			}
+			
 		}
+		
+		System.out.println(exclusionList);
 	}
+	
 
+	/**
+	 * A comparator class. Used for sorting sentences by their ranks
+	 * 
+	 * @author Henry
+	 *
+	 */
 	private class CompareByRank implements Comparator<NewsFragment> {
 		@Override
 		public int compare(NewsFragment o1, NewsFragment o2) {
@@ -213,20 +367,18 @@ public class NewsParser {
 		}
 	}
 
+	/**
+	 * A comparator class. Used for sorting sentences by their Id (written order
+	 * in the article)
+	 * 
+	 * @author Henry
+	 *
+	 */
 	private class CompareById implements Comparator<NewsFragment> {
 		@Override
 		public int compare(NewsFragment o1, NewsFragment o2) {
 			return o1.getId() - o2.getId();
 		}
-	}
-
-	public String getHtmlContents() {
-		String temp = new String();
-		for (String s : content) {
-			temp += s;
-			temp += "\n\n";
-		}
-		return temp;
 	}
 
 	public boolean isUrlValid() {
@@ -236,6 +388,22 @@ public class NewsParser {
 	public void printContents() {
 		for (NewsFragment s : contents) {
 			System.out.println(s.getSentence());
+		}
+	}
+	
+	public void printExcludedWordList(){
+		System.out.println();
+		System.out.println("The excluded word list is as follows: ");
+		for (String s : exclusionList){
+			System.out.println("\u2022 " + s);
+		}
+	}
+	
+	public void printFrequentWordList(){
+		System.out.println();
+		System.out.println("The frequent word list is as follows: ");
+		for (String s : frequentWords){
+			System.out.println("\u2022 " + s);
 		}
 	}
 
